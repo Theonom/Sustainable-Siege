7c9d3b5ed18129268ee63657272e8d61f1d78d69
rizkiwijanarko <rizkiwijanarko@students.unnes.ac.id>
2023-06-20 23:56:36 +0700

    fix bug camera

--- Sustainable Siege/Assets/ThirdPersonController.cs
+++ Sustainable Siege/Assets/ThirdPersonController.cs
@@ -1,44 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ThirdPersonController : MonoBehaviour
 {
-    public CharacterController controller;
+    public Transform player;
     public Transform cam;
 
-    public float speed;
-    public float rotatioSmoothTime;
-    private float rotationVelocity;
-    private float targetRotation;
-    private float targetSpeed;
+    public float speed = 6.0f;
+    public float rotationSmoothTime = 0.12f;
+    public float cameraRotationSpeed = 2.0f;
 
-    void Update()
+    private CharacterController controller;
+    private Vector3 moveDirection;
+    private float rotationSmoothVelocity;
+    private float targetRotation;
+
+    private void Start()
     {
-        Move();
+        controller = GetComponent<CharacterController>();
     }
 
-    private void Move()
+    private void Update()
+    {
+        HandleMovement();
+        HandleCameraRotation();
+    }
+
+    private void HandleMovement()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-
         Vector3 direction = new Vector3(horizontal, 0.0f, vertical).normalized;
-
-        if (direction.magnitude == 0.0f)
-        {
-            targetSpeed = 0.0f;
-        }
 
         if (direction.magnitude >= 0.1f)
         {
-            targetSpeed = speed;
-            targetRotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref rotationVelocity, rotatioSmoothTime);
-            transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
-        }
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            targetRotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref rotationSmoothVelocity, rotationSmoothTime);
 
-        Vector3 targetDirection = Quaternion.Euler(0.0f, targetRotation, 0.0f) * Vector3.forward;
-        controller.Move(targetDirection.normalized * targetSpeed * Time.deltaTime);
+            if (vertical > 0.0f)
+            {
+                transform.rotation = Quaternion.Euler(0.0f, targetRotation, 0.0f);
+            }
+
+            moveDirection = Quaternion.Euler(0.0f, targetAngle, 0.0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+    }
+
+    private void HandleCameraRotation()
+    {
+        float cameraRotationInput = Input.GetAxis("Mouse X") * cameraRotationSpeed;
+        cam.RotateAround(player.position, Vector3.up, cameraRotationInput);
     }
 }
